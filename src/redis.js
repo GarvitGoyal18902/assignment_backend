a027d21a32e4a9bf51a84d2d257e6b2639db14be
Tofiq Quadri <tofiqquadri@ymail.com>
2026-03-28 20:10:35 +0530

    Update Redis

--- src/redis.js
+++ src/redis.js
@@ -1,13 +1,13 @@
 const { createClient } = require('redis');
 
-const redisClient = createClient();
+const redisClient = createClient({ url: process.env.REDIS_URL });
 
 redisClient.on('error', (err) => {
     console.error('Redis error:', err);
 });
 
-(async () => {
-    await redisClient.connect();
-})();
+redisClient.connect().catch((err) => {
+    console.error('Redis connect failed:', err);
+});
 
 module.exports = { redisClient };
