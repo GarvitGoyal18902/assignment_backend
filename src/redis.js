1e8be6d7f074cb85b07b507cf597164659a93bf4
GarvitGoyal18902 <garvit18902@gmail.com>
2026-03-29 12:42:09 +0530

    redis url fix

--- src/redis.js
+++ src/redis.js
@@ -1,6 +1,6 @@
 const { createClient } = require('redis');
 
-const redisClient = createClient({ url: process.env.REDIS_URL });
+const redisClient = createClient({ url: process.env.poll_web_REDIS_URL });
 
 redisClient.on('error', (err) => {
     console.error('Redis error:', err);
